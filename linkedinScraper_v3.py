c0c29f166d111c74299c951545dfa3d4c071f9b2
Josh Rainbow <joshrainbow@gmail.com>
2023-02-06 13:15:10 -0700

    init

---
+++ linkedinScraper_v3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 6 12:12:47 2023
+@author: joshrainbow
+
+"""
+################################# Imports ################################
+# Imports for scraping
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+
+# Date and time related imports
+import time
+from datetime import datetime
+
