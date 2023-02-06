a26848006b086ade6fc3db30f6fef37ec7a31520
Josh Rainbow <joshrainbow@gmail.com>
2023-02-06 13:21:41 -0700

    opening the website

--- linkedinScraper_v3.py
+++ linkedinScraper_v3.py
@@ -16,3 +16,23 @@ from selenium.webdriver.common.by import By
 import time
 from datetime import datetime
 
+# Libraries needed for moving files around
+from pathlib import Path
+import glob
+import os
+import zipfile
+
+# Assign the chrome driver to a variable
+driver = webdriver.Chrome("/Users/mutumbo/Desktop/BTG/chromeDriver/chromedriver")
+
+####################### Opening the Website #########################
+# Go to the webpage of interest
+driver.get("https://www.linkedin.com/jobs/")
+print(driver.title)
+
+# Wait for the page to load
+page_load_wait_time = 5 # in seconds
+# Wait up to 120s before assuming something is wrong with the download
+max_wait_time_for_download = 120 
+time.sleep(page_load_wait_time) # wait a certain number of seconds
+
