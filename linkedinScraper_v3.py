c0d957c16a0936d09b2ccdb22d214ae8a322eaea
Josh Rainbow <joshrainbow@gmail.com>
2023-02-09 16:41:33 -0700

    added jobs loop to segment job posting parts

--- linkedinScraper_v3.py
+++ linkedinScraper_v3.py
@@ -12,29 +12,51 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-
+'''
 # setting URL
-url = 'https://www.linkedin.com/jobs/search?keywords=Data+Science&location=Canada&geoId=&trk=public_jobs_jobs-search-bar_search-submit&original_referer=https://www.linkedin.com/jobs/search?keywords=data%20science&location=Calgary%2C%20Alberta%2C%20Canada&geoId=102199904&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&original_refer'
+url = 'https://www.linkedin.com/jobs/search?keywords=Data+Science&location=Canada&geoId=&trk=public_jobs_jobs-search-bar_search-submit&original_referer=https://www.linkedin.com/jobs/search?keywords=data%20science&location=Calgary%2C%20Alberta%2C%20Canada&geoId=102199904&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0'
 
 response = requests.get(url)
 print(response)
 
-'''
+
 before we can start extracting any data from the page, we need to parse the raw HTML data
 making it easier to navigate using CSS selectors
-'''
+
 
 # create a BS4 object by passing response.content as the first argument
 soup = BeautifulSoup(response.content, 'html.parser')
 
 jobTitle = soup.find('h3', class_='base-search-card__title').text
-print(jobTitle)
+company = soup.find('h4', class_='base-search-card__subtitle-link').text
+#print(jobTitle)
 
-'''
+
+
 now we need to get around the problem of moving through the pages
 '''
 def linkedinScraper(webpage, pageNumber):
     nextPage = webpage + str(pageNumber)
-    print(str(pageNumber))
+    #print(str(pageNumber))
     response = requests.get(nextPage)
     soup = BeautifulSoup(response.content, 'html.parser')
+    print(nextPage)
+    print(response)
+    print(pageNumber)
+    
+
+    if pageNumber < 25:
+        pageNumber = pageNumber + 25
+        linkedinScraper(webpage, pageNumber)
+    
+    jobs = soup.find_all('div', class_='base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card')
+    
+    for job in jobs:
+        jobTitle = job.find('h3', class_='base-search-card__title').text.strip()
+        jobCompany = job.find('h4', class_='base-search-card__subtitle').text.strip()
+        jobLocation = job.find('span', class_='job-search-card__location').text.strip()
+        jobLink = job.find('a', class_='base-card__full-link')['href']
+        
+
+linkedinScraper('https://www.linkedin.com/jobs/search?keywords=Data+Science&location=Canada&geoId=&trk=public_jobs_jobs-search-bar_search-submit&original_referer=https://www.linkedin.com/jobs/search?keywords=data%20science&location=Calgary%2C%20Alberta%2C%20Canada&geoId=102199904&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&start=', 0)
+
