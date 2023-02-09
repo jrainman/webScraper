c7680c3b87dc7004373d44be0710ba90f1d1468a
Josh Rainbow <joshrainbow@gmail.com>
2023-02-09 16:45:18 -0700

    added in csv writer and closing function

--- linkedinScraper_v3.py
+++ linkedinScraper_v3.py
@@ -35,6 +35,11 @@ company = soup.find('h4', class_='base-search-card__subtitle-link').text
 
 now we need to get around the problem of moving through the pages
 '''
+
+file = open('linkedinJobs.csv', 'a')
+writer = csv.writer(file)
+writer.writerow(['Title', 'Company', 'Location', 'Link'])
+
 def linkedinScraper(webpage, pageNumber):
     nextPage = webpage + str(pageNumber)
     #print(str(pageNumber))
@@ -44,11 +49,6 @@ def linkedinScraper(webpage, pageNumber):
     print(response)
     print(pageNumber)
     
-
-    if pageNumber < 25:
-        pageNumber = pageNumber + 25
-        linkedinScraper(webpage, pageNumber)
-    
     jobs = soup.find_all('div', class_='base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card')
     
     for job in jobs:
@@ -56,7 +56,20 @@ def linkedinScraper(webpage, pageNumber):
         jobCompany = job.find('h4', class_='base-search-card__subtitle').text.strip()
         jobLocation = job.find('span', class_='job-search-card__location').text.strip()
         jobLink = job.find('a', class_='base-card__full-link')['href']
-        
+    
+    writer.writerow([jobTitle.encode('utf-8'), 
+                     jobCompany.encode('utf-8'), 
+                     jobLocation.encode('utf-8'), 
+                     jobLink.encode('utf-8')])
+    
+    print("Data written to CSV")
+    
+    if pageNumber < 25:
+        pageNumber = pageNumber + 25
+        linkedinScraper(webpage, pageNumber)
+    else:
+        file.close()
+    print("File closed")
 
 linkedinScraper('https://www.linkedin.com/jobs/search?keywords=Data+Science&location=Canada&geoId=&trk=public_jobs_jobs-search-bar_search-submit&original_referer=https://www.linkedin.com/jobs/search?keywords=data%20science&location=Calgary%2C%20Alberta%2C%20Canada&geoId=102199904&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&start=', 0)
 
